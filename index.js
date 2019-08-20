a7f827d57afe1c90d746ddbeadefb81fd7072647
Igor Souza <igorsouza.dev@gmail.com>
2019-08-19 21:06:43 -0300

    Added projects CRUD

--- index.js
+++ index.js
@@ -4,8 +4,48 @@ const server = express();
 
 server.use(express.json());
 
-server.get("/", (req, res) => {
-  return res.json({ message: "Hello World!" });
+const projects = [];
+const getProjectIndex = id => {
+  let i = 0;
+  for (project of projects) {
+    if (project.id === id) {
+      return i;
+    }
+    i++;
+  }
+  return null;
+};
+const checkProjectExists = (req, res, next) => {
+  const { id } = req.params;
+  if (!getProjectIndex(id)) {
+    return res.status(400).json({ error: "Project does not exists" });
+  }
+  return next();
+};
+
+server.get("/projects", (req, res) => {
+  return res.json(projects);
+});
+
+server.post("/projects", (req, res) => {
+  const { id, title } = req.body;
+  projects.push({ id, title });
+
+  return res.json(projects);
+});
+
+server.put("/projects/:id", checkProjectExists, (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+  projects[getProjectIndex(id)].title = title;
+
+  return res.json(projects);
+});
+
+server.delete("/projects/:id", checkProjectExists, (req, res) => {
+  const { id } = req.params;
+  projects.splice(getProjectIndex(id), 1);
+  return res.send();
 });
 
 server.listen(3000);
